0bcde052b69ac7d21e00f38bb816072608dc3865
“poncedavid” <Da.ponce@duocuc.cl>
2024-07-18 20:07:21 -0400

    introduccion a python

---
+++ fundamentos/intro/introduccion.js
@@ -0,0 +1,12 @@
+console.log("hola mundo desde JS")
+
+//Este es un comentario
+
+
+function suma(a ,b){
+
+    return a + b;
+}
+
+
+console.log(suma(5,5))
---
+++ fundamentos/intro/introduccion.py
@@ -0,0 +1,45 @@
+print("Hola mundo mundo desde Python")
+
+#Este es un comentario
+'''
+este es
+un comentario de varias lineas
+'''
+
+# variables y tipos de datos
+
+entero = 10
+flotante = 10.5
+cadena = "Juan Perez"
+booleano = False
+
+nombre= "juan"
+apellido = "perez"
+edad = 30
+
+
+#Como imprimir variables y concatenar
+
+#print("Hola mi nombre es" , nombre, apellido, " y tengo", edad, "años")
+
+#print(f"Hola mi nombre es {nombre} {apellido} y tengo {edad } años")
+
+
+# Operadores
+
+#suma +
+#resta -
+#multiplicacion *
+#division /
+#division entera //
+#modulo %
+#potencia **
+
+num1 = 3
+
+num2 = 10
+
+
+
+
+print(num1 % num2)
