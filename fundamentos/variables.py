367014391c20041c88db044eb4973d926bdbafaf
poncedavid <da.ponce@duocuc.cl>
2024-07-25 19:17:14 -0400

    variables.

---
+++ fundamentos/variables.py
@@ -0,0 +1,35 @@
+# Declarar las variables.
+
+#Cadenas o Strings
+nombre = "Coding Dojo"
+
+#Enteros
+edad = 25
+
+#Flotantes
+valor_pi = 3.14
+
+#Booleanos
+tiene_hambre = True
+tiene_sueño = False
+
+
+#Tipos de datos compuestos.
+
+#Listas
+
+lista_de_compras = ["CodinDojo",25,3.14,True,False, ["0",1,2,3]]
+
+#Diccionarios
+#Un diccionario tiene claves y valores
+diccionario = {
+    "Nombre":"Coding Dojo",
+    "Edad": 25,
+    "ValorPi": 3.14,
+    "TieneHambre": True
+}
+
+
+print("Mi nombre es "+ nombre + "y tengo:",edad)
+
+print(f"Mi nombre es {nombre} y tengo {edad} años")
