7a2e7e5dece2e7b8ccd5a2fb52a27e12b5759988
poncedavid <da.ponce@duocuc.cl>
2024-07-25 20:16:57 -0400

    ejercicios for

---
+++ fundamentos/ejemplo_for.py
@@ -0,0 +1,18 @@
+# #Iteracion del 0 al 100
+# for i in range(101):
+#     print(i)
+
+
+# #Multiplos de dos
+# for i in range(2,501,2):
+#     print(i)
+
+
+#Contando modulos
+for i in range(1,101):
+    if i % 10 == 0:
+        print("Baby")
+    elif i % 5 == 0:
+        print("ice ice")
+    else:
+        print(i)
